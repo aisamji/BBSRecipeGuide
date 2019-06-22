3584eda540d2d93c65390158e7b3a260939164a8
Ali I Samji <ali.samji@outlook.com>
2019-06-22 16:40:27 -0500

    Used placeholder loading screens.

--- app/src/main/java/toys/aisamji/bbsrecipeguide/recipe/RecipeDetailActivity.kt
+++ app/src/main/java/toys/aisamji/bbsrecipeguide/recipe/RecipeDetailActivity.kt
@@ -6,24 +6,36 @@ import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.TableRow
 import android.widget.TextView
+import kotlinx.coroutines.*
 import toys.aisamji.bbsrecipeguide.*
 import toys.aisamji.bbsrecipeguide.data.Command
 
 class RecipeDetailActivity : AppCompatActivity() {
 
-    private lateinit var ingredientViewA: TextView
-    private lateinit var ingredientViewB: TextView
+    private val ingredientViewA: TextView
+        get() = findViewById(R.id.recipe_detail_ingredient_1)
+    private val ingredientViewB: TextView
+        get() = findViewById(R.id.recipe_detail_ingredient_2)
 
-    private lateinit var altResultViewA: TableRow
-    private lateinit var altResultViewB: TableRow
+    private val altResultViewA: TableRow
+        get() = findViewById(R.id.recipe_detail_alt_1)
+    private val altResultViewB: TableRow
+        get() = findViewById(R.id.recipe_detail_alt_2)
 
-    private lateinit var shimmeringView: TextView
-    private lateinit var fleetingView: TextView
-    private lateinit var pulsingView: TextView
-    private lateinit var wellspringView: TextView
-    private lateinit var soothingView: TextView
-    private lateinit var hungryView: TextView
-    private lateinit var aboundingView: TextView
+    private val shimmeringView: TextView
+        get() = findViewById(R.id.recipe_detail_shimmering)
+    private val fleetingView: TextView
+        get() = findViewById(R.id.recipe_detail_fleeting)
+    private val pulsingView: TextView
+        get() = findViewById(R.id.recipe_detail_pulsing)
+    private val wellspringView: TextView
+        get() = findViewById(R.id.recipe_detail_wellspring)
+    private val soothingView: TextView
+        get() = findViewById(R.id.recipe_detail_soothing)
+    private val hungryView: TextView
+        get() = findViewById(R.id.recipe_detail_hungry)
+    private val aboundingView: TextView
+        get() = findViewById(R.id.recipe_detail_abounding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,68 +44,74 @@ class RecipeDetailActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        ingredientViewA = findViewById(R.id.recipe_detail_ingredient_1)
-        ingredientViewB = findViewById(R.id.recipe_detail_ingredient_2)
-
-        altResultViewA = findViewById(R.id.recipe_detail_alt_1)
-        altResultViewB = findViewById(R.id.recipe_detail_alt_2)
-
-        shimmeringView = findViewById(R.id.recipe_detail_shimmering)
-        fleetingView = findViewById(R.id.recipe_detail_fleeting)
-        pulsingView = findViewById(R.id.recipe_detail_pulsing)
-        wellspringView = findViewById(R.id.recipe_detail_wellspring)
-        soothingView = findViewById(R.id.recipe_detail_soothing)
-        hungryView = findViewById(R.id.recipe_detail_hungry)
-        aboundingView = findViewById(R.id.recipe_detail_abounding)
-    }
-
     override fun onResume() {
         super.onResume()
 
-        val ingredientA = commands[intent.getIntExtra(extraIngredientA, 0)]
-        val ingredientB = commands[intent.getIntExtra(extraIngredientB, 0)]
+        GlobalScope.launch {
+            val ingredientA = commands[intent.getIntExtra(extraIngredientA, 0)]
+            val ingredientB = commands[intent.getIntExtra(extraIngredientB, 0)]
 
-        ingredientViewA.text = ingredientA.name
-        ingredientViewB.text = ingredientB.name
+            MainScope().launch {
+                ingredientViewA.text = ingredientA.name
+                ingredientViewB.text = ingredientB.name
 
-
-
-        val alternatives = recipes.forWielder(filter.wielder).involvingBothOf(ingredientA, ingredientB).get()
-
-        val alt1 = commands[alternatives.first().resultId]
-
-        altResultViewA.let {
-            it.findViewWithTag<TextView>("name").text = alt1.name
-            it.findViewWithTag<TextView>("name").setTextColor(calculateColorFor(alt1))
-            it.findViewWithTag<TextView>("chance").text = getString(R.string.chance, (alternatives.first().successRate * 100).toInt())
-        }
-
-        altResultViewB.let {
-            if (alternatives.count() == 2) {
-                val alt2 = commands[alternatives.last().resultId]
-
-                it.findViewWithTag<TextView>("name").text = alt2.name
-                it.findViewWithTag<TextView>("name").setTextColor(calculateColorFor(alt2))
-                it.findViewWithTag<TextView>("chance").text = getString(R.string.chance, (alternatives.last().successRate * 100).toInt())
-            } else {
-                it.visibility = View.GONE
+                ingredientViewA.background = null
+                ingredientViewB.background = null
             }
-        }
+
+            val alternatives = recipes.forWielder(filter.wielder).involvingBothOf(ingredientA, ingredientB).get()
+            val alt1 = commands[alternatives.first().resultId]
+
+            altResultViewA.let {
+                MainScope().launch {
+                    it.findViewWithTag<TextView>("name").text = alt1.name
+                    it.findViewWithTag<TextView>("name").setTextColor(calculateColorFor(alt1))
+                    it.findViewWithTag<TextView>("chance").text = getString(R.string.chance, (alternatives.first().successRate * 100).toInt())
+                    it.background = null
+                }
+            }
+
+            altResultViewB.let {
+                if (alternatives.count() == 2) {
+                    val alt2 = commands[alternatives.last().resultId]
+                    val alt2Color = calculateColorFor(alt2)
+
+                    MainScope().launch {
+                        it.findViewWithTag<TextView>("name").text = alt2.name
+                        it.findViewWithTag<TextView>("name").setTextColor(alt2Color)
+                        it.findViewWithTag<TextView>("chance").text = getString(R.string.chance, (alternatives.last().successRate * 100).toInt())
+                        it.background = null
+                        it.visibility = View.VISIBLE
+                    }
+                } else {
+                    MainScope().launch {
+                        it.visibility = View.GONE
+                    }
+                }
+            }
 
 
-        val recipeType = recipeTypes[alternatives.first().typeId] ?: recipeTypes[alternatives.last().typeId]!!
+            val recipeType = recipeTypes[alternatives.first().typeId] ?: recipeTypes[alternatives.last().typeId]!!
 
-        recipeType.let {
-            shimmeringView.text = abilities[it.shimmeringResultId].name
-            fleetingView.text = abilities[it.fleetingResultId].name
-            pulsingView.text = abilities[it.pulsingResultd].name
-            wellspringView.text = abilities[it.wellspringResultId].name
-            soothingView.text = abilities[it.soothingResultId].name
-            hungryView.text = abilities[it.hungryResultId].name
-            aboundingView.text = abilities[it.aboundingResultId].name
+            recipeType.let {
+                MainScope().launch {
+                    shimmeringView.text = abilities[it.shimmeringResultId].name
+                    fleetingView.text = abilities[it.fleetingResultId].name
+                    pulsingView.text = abilities[it.pulsingResultd].name
+                    wellspringView.text = abilities[it.wellspringResultId].name
+                    soothingView.text = abilities[it.soothingResultId].name
+                    hungryView.text = abilities[it.hungryResultId].name
+                    aboundingView.text = abilities[it.aboundingResultId].name
+
+                    shimmeringView.background = null
+                    fleetingView.background = null
+                    pulsingView.background = null
+                    wellspringView.background = null
+                    soothingView.background = null
+                    hungryView.background = null
+                    aboundingView.background = null
+                }
+            }
         }
     }
 
@@ -107,7 +125,7 @@ class RecipeDetailActivity : AppCompatActivity() {
     }
 
     companion object {
-        val extraIngredientA = "first-ingredient"
-        val extraIngredientB = "second-ingredient"
+        const val extraIngredientA = "first-ingredient"
+        const val extraIngredientB = "second-ingredient"
     }
 }
